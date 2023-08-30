047d5d2252d340bab82cceb27017ca1dd6c17fa4
jmk445 <jmk445123456@gmail.com>
2023-08-30 15:51:37 +0900

    feat : review 페이지 get api 추가

--- app/server/routes/product_review.py
+++ app/server/routes/product_review.py
@@ -6,6 +6,9 @@ from server.models.product_review import ProductReview, UpdateProductReview
 
 
 router = APIRouter()
+@router.get("/", tags=["product_reviews"])
+def get_product_review():
+    return {"message" : "hey there! you've got into the review page"}
 
 @router.post("/", response_description="Review added to the database")
 async def add_product_review(review: ProductReview) -> dict:
